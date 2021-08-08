7f6355c87d6c15e22f46cef5bbd1d28889a84437
Aras Opraza <34035232+arasopraza@users.noreply.github.com>
2021-08-08 16:05:50 +0700

    add validation menu & kuah

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -9,61 +9,80 @@ use Auth;
 
 class MenuController extends Controller
 {
-    public function show(){        
-        $menu = Menu::all();      
-        $kuah = Kuah::all();             
-        if(Auth::user()->role == 'admin'){            
+    public function show()
+    {
+        $menu = Menu::all();
+        $kuah = Kuah::all();
+        if (Auth::user()->role == 'admin') {
             return view('admin.menu', compact('menu', 'kuah'));
-        }else if(Auth::user()->role == 'kasir'){
+        } else if (Auth::user()->role == 'kasir') {
             return view('kasir.menu', compact('menu', 'kuah'));
-        }        
+        }
     }
 
-    public function create(){
-        
+    public function create()
+    {
     }
 
-    public function storeMenu(Request $req){
-       Menu::create([
+    public function storeMenu(Request $req)
+    {
+        $req->validate([
+            'kategori' => 'required',
+            'nama' => 'required',
+            'harga' => 'required',
+        ]);
+
+        Menu::create([
             'kategori' => $req->kategori,
             'nama' => $req->nama,
             'harga' => $req->harga
-       ]);       
-    }
-
-    public function storeKuah(Request $req){        
-        Kuah::create([
-             'nama' => $req->nama
         ]);
-     }
-
-    public function editMenu($id){
-        $menu = Menu::findOrFail($id);        
     }
 
-    public function editKuah($id){
+    public function storeKuah(Request $req)
+    {
+        $req->validate([
+            'nama' => 'required',
+        ]);
+
+        Kuah::create([
+            'nama' => $req->nama
+        ]);
+    }
+
+    public function editMenu($id)
+    {
+        $menu = Menu::findOrFail($id);
+    }
+
+    public function editKuah($id)
+    {
         $kuah = Kuah::findOrFail($id);
     }
 
-    public function updateMenu(Request $req, $id){
-       $menu = Menu::findOrFail($id);       
-       $menu->kategori = $req->kategori;
-       $menu->nama = $req->nama;
-       $menu->harga = $req->harga;
-       $menu->save();
+    public function updateMenu(Request $req, $id)
+    {
+        $menu = Menu::findOrFail($id);
+        $menu->kategori = $req->kategori;
+        $menu->nama = $req->nama;
+        $menu->harga = $req->harga;
+        $menu->save();
     }
 
-    public function updateKuah(Request $req, $id){        
+    public function updateKuah(Request $req, $id)
+    {
         $kuah = Kuah::findOrFail($id);
         $kuah->nama = $req->nama;
         $kuah->save();
-     }
+    }
 
-    public function destroyMenu($id){
+    public function destroyMenu($id)
+    {
         Menu::findOrFail($id)->delete();
     }
 
-    public function destroyKuah($id){
+    public function destroyKuah($id)
+    {
         Kuah::findOrFail($id)->delete();
     }
 }
